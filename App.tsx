aaf8d2e134bbd33ee88333f0259d0db2f015c6d3
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2023-04-26 13:58:15 -0300

    Squashed commit of the following:
    commit 6648fae379f7793a7ead3bd1046ea3ee88a8038c
    Author: Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
    Date:   Wed Apr 26 13:28:19 2023 -0300
    
        App.tsx
    
    commit 3997628afb0487536da7d3bd95211607babb5abd
    Author: Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
    Date:   Wed Apr 26 11:08:05 2023 -0300
    
        App.tsx
    
    commit 7e557ab5c581593cf39bb05ee93778acbc12151a
    Author: Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
    Date:   Wed Apr 26 11:03:54 2023 -0300
    
        App.tsx
    
    commit e9e4543c353d315429a11d6c6de95cc2af37b1b6
    Author: Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
    Date:   Wed Apr 26 10:39:32 2023 -0300
    
        App.tsx
        package.json
        yarn.lock
    
    commit 67f4c31117ebcc79d45e74cc77d594ddc3ad50ae
    Author: Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
    Date:   Wed Apr 26 09:49:37 2023 -0300
    
        App.tsx
        package.json
        yarn.lock

--- App.tsx
+++ App.tsx
@@ -1,7 +1,29 @@
-import React, {useState} from 'react';
-import {Button, Pressable, SafeAreaView, Text, View} from 'react-native';
-import {A11yOrder, A11yProvider, useFocusOrder} from 'react-native-a11y';
-import Modal from 'react-native-modal';
+/* eslint-disable react-native/no-inline-styles */
+
+import {faker} from '@faker-js/faker/locale/pt_BR';
+import _ from 'lodash';
+import React, {
+  RefCallback,
+  RefObject,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import {
+  Button,
+  Keyboard,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import {
+  A11yModule,
+  A11yOrder,
+  A11yProvider,
+  useFocusOrder,
+} from 'react-native-a11y';
 
 export default () => {
   return (
@@ -10,49 +32,141 @@ export default () => {
     </A11yProvider>
   );
 };
-function App() {
-  const [isModalVisible, setModalVisible] = useState(false);
 
-  const toggleModal = () => {
-    setModalVisible(!isModalVisible);
-  };
+type Message = {
+  content: string;
+  position: 'left' | 'right';
+};
 
-  const {a11yOrder, refs} = useFocusOrder(3);
+function Message({
+  content,
+  position,
+  a11yOrderRef: ref,
+  lastMessageRef,
+}: Message & {
+  a11yOrderRef: RefCallback<View>;
+  lastMessageRef: RefObject<View>;
+}) {
   return (
-    <SafeAreaView>
-      <Modal isVisible={isModalVisible}>
-        <View style={{backgroundColor: 'white'}}>
-          <Text>Paragrafo um</Text>
-          <Text>Segundo parágrafo</Text>
-          <Button title="Esconder" onPress={toggleModal} />
-        </View>
-      </Modal>
-      <View>
-        <A11yOrder a11yOrder={a11yOrder}>
-          <Pressable
-            onPress={toggleModal}
-            style={{backgroundColor: 'red', height: 100, width: 100}}
-            accessibilityLabel="Segundo"
-            ref={refs[1]}
-          />
-          <View style={{height: 100}} />
-          <View
-            style={{backgroundColor: 'blue', height: 100, width: 100}}
-            accessibilityLabel="Primeiro"
-            accessibilityLanguage="en"
-            ref={refs[0]}
-          />
-          <View style={{height: 100}} />
-          <View
-            style={{backgroundColor: 'green', height: 100, width: 100}}
-            accessibilityLabel="Terceiro"
-            ref={refs[2]}
-          />
-        </A11yOrder>
-        <View style={{height: 100}} />
-        <Text>Some text</Text>
-        <Text accessibilityLanguage="pt">Meu nome é Leonardo</Text>
+    <View
+      ref={e => {
+        ref?.(e);
+        // @ts-expect-error
+        lastMessageRef!.current = e;
+      }}
+      accessible
+      style={{padding: 10, flexDirection: 'row'}}>
+      {position === 'right' && <View style={{width: '20%'}} />}
+      <View
+        style={{
+          padding: 10,
+          flex: 1,
+          borderWidth: 1,
+        }}>
+        <Text style={{textAlign: position}}>{content}</Text>
       </View>
+      {position === 'left' && <View style={{width: '20%'}} />}
+    </View>
+  );
+}
+
+function App() {
+  const [messages, setMessages] = useState<Message[]>(
+    Array.from({length: 10}).map(() => createRandomMessage()),
+  );
+  useEffect(() => {
+    scrollViewRef.current?.scrollToEnd({animated: false});
+  }, []);
+  const [input, setInput] = useState('');
+  const scrollViewRef = useRef<ScrollView>(null);
+  const lastMessageRef = useRef<View>(null);
+
+  const {a11yOrder, refs} = useFocusOrder(5 + messages.length);
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <A11yOrder
+        style={{flex: 1}}
+        a11yOrder={{
+          ...a11yOrder,
+          onLayout(...args) {
+            a11yOrder.onLayout(...args);
+          },
+        }}>
+        <Text ref={refs[0]}>Você tem uma urgência</Text>
+        <View style={{height: 20}} />
+        <View ref={refs[1]} accessible>
+          <Text>Leonardo Guarnieri de Bastiani</Text>
+          <Text>Companheiro</Text>
+        </View>
+        <View style={{height: 20}} />
+        <ScrollView
+          ref={ref => {
+            // @ts-expect-error
+            scrollViewRef.current = ref;
+            refs[4](ref);
+          }}
+          style={{flex: 1}}>
+          {messages.map((message, index) => (
+            <Message
+              lastMessageRef={lastMessageRef}
+              a11yOrderRef={refs[index + 5]}
+              key={index}
+              content={message.content}
+              position={message.position}
+            />
+          ))}
+        </ScrollView>
+        <View style={{paddingHorizontal: 20, flexDirection: 'row'}}>
+          <TextInput
+            ref={refs[2]}
+            style={{
+              borderColor: 'black',
+              borderWidth: 1,
+              flex: 1,
+            }}
+            placeholder="Escreva sua mensagem..."
+            value={input}
+            onChangeText={setInput}
+          />
+          <Button
+            title="💌"
+            ref={refs[3]}
+            onPress={() => {
+              if (input) {
+                setMessages(currentMessages => [
+                  ...currentMessages,
+                  {
+                    content: input,
+                    position: 'right',
+                  },
+                ]);
+              } else {
+                setMessages(currentMessages => [
+                  ...currentMessages,
+                  createRandomMessage(),
+                ]);
+              }
+              scrollViewRef.current?.scrollToEnd();
+              setInput('');
+              Keyboard.dismiss();
+              requestAnimationFrame(() => {
+                requestAnimationFrame(() => {
+                  requestAnimationFrame(() => {
+                    A11yModule.setA11yFocus(lastMessageRef);
+                  });
+                });
+              });
+            }}
+          />
+        </View>
+        <View style={{height: 20}} />
+      </A11yOrder>
     </SafeAreaView>
   );
+}
+function createRandomMessage(): Message {
+  return {
+    content: faker.hacker.phrase(),
+    position: _.sample(['left', 'right'])!,
+  };
 }
